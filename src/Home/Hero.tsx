6fc088375a931ffe5a94c6dcc092b4d58809b7ce
Bruno Leonardo Michels <brunolm7@gmail.com>
2023-03-19 13:12:05 -0300

    Add github

--- src/Home/Hero.tsx
+++ src/Home/Hero.tsx
@@ -48,6 +48,36 @@ export const Hero = () => {
             <img alt="brunolm on codewars" src="https://www.codewars.com/users/brunolm/badges/micro" height="28px" />
           </div>
           <p>Bruno has over 96k reputation points on Stack Overflow and reached rank 2 on CodeWars.</p>
+          <div class="flex flex-col justify-center text-center pt-6">
+            <div class="flex justify-center text-center">
+              <img
+                alt=""
+                src="https://github-readme-stats.vercel.app/api?username=brunolm&hide=contribs,prs,issues,commits&theme=tokyonight&show_icons=true"
+              />
+            </div>
+            <span class="pt-4">
+              <a
+                aria-label="brunolm on github"
+                href="https://github.com/brunolm"
+                target="_blank"
+                class="text-indigo-500 flex items-center justify-center gap-2"
+              >
+                <svg
+                  class="h-8 w-8 text-indigo-500"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  {' '}
+                  <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
+                </svg>
+                <span>brunolm</span>
+              </a>
+            </span>
+          </div>
         </div>
       </section>
     </div>
